f956a9ea0685dc1aaa0ab7146735e0e3a5449889
thrust0 <dragforce@proton.me>
2026-05-08 19:50:29 +0300

    cross reference table function added

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@
 #include <stdexcept>
 #include <sstream>
 #include <vector>
+#include <cctype>
+#include <set>
 
+/*
+TODO
+cross reference lentele
+*/
 const std::vector<std::string> FILE_PATHS = {
     "input/sample.txt",
-    "output/output.txt"
+    "output/output.txt",
+    "output/cross-reference-table.txt"
 };
 
 const std::vector<char> punctuation = {
@@ -27,23 +34,21 @@ const std::vector<char> punctuation = {
     '"'
 };
 
-/*
-TODO:
-
-*/
-
-void file_input(std::map<std::string, int>& words, const std::string filename);
-void file_output(const std::map<std::string, int>& words, const std::string filename);
+void file_input(std::map<std::string, int>& words, std::map<std::string, std::set<int>>& location, const std::string filename);
+void word_count_output(const std::map<std::string, int>& words, const std::string filename);
+void cross_reference_table_output(const std::map<std::string, std::set<int>>& location, const std::map<std::string, int>& words, std::string filename);
 
 int main()
 {
-    std::map<std::string, int> words;
-    file_input(words, FILE_PATHS.at(0));
-    file_output(words, FILE_PATHS.at(1));
+    std::map<std::string, int> words;                       //map for duplicate words
+    std::map<std::string, std::set<int>> location;          //cross reference table which stores the line where the duplicate words are
+    file_input(words, location, FILE_PATHS.at(0));
+    word_count_output(words, FILE_PATHS.at(1));
+    cross_reference_table_output(location, words, FILE_PATHS.at(2));
     return 0;
 }
 
-void file_input(std::map<std::string, int>& words, const std::string filename)
+void file_input(std::map<std::string, int>& words, std::map<std::string, std::set<int>>& location, const std::string filename)
 {
     std::ifstream ist {filename};
     if(!ist)
@@ -51,7 +56,10 @@ void file_input(std::map<std::string, int>& words, const std::string filename)
         throw std::runtime_error("Neisejo atidaryti failo irasymui...");
         return;
     }
+
     std::string line;
+    int line_count = 1;
+
     while(std::getline(ist, line))
     {
         std::stringstream ss{line};
@@ -60,19 +68,24 @@ void file_input(std::map<std::string, int>& words, const std::string filename)
         {
             for(int i = 0; i < temp.size(); i++)
             {
-                for(int j = 0; j < punctuation.size(); j++)
+                if(std::isupper(temp[i]))                   //check if character ir uppercase, if yes -> make it lower case
+                    temp[i] = std::tolower(temp[i]);
+                
+                for(int j = 0; j < punctuation.size(); j++)         //remove punctuation
                 {
                     if(temp[i] == punctuation.at(j))
                         temp.erase(i);
                 }
             }
             ++words[temp]; 
+            location[temp].insert(line_count);
         }
+        ++line_count;
     }
     ist.close();
 }
 
-void file_output(const std::map<std::string, int>& words, const std::string filename)
+void word_count_output(const std::map<std::string, int>& words, const std::string filename)
 {
     std::ofstream ost{filename};
     if(!ost)
@@ -82,7 +95,28 @@ void file_output(const std::map<std::string, int>& words, const std::string file
     }
 
     for(const auto& [key, value] : words)
-        if(value > 0)
+        if(value > 1)
             ost << key << ": " << value << "\n";
-    
+        
+}
+
+void cross_reference_table_output(const std::map<std::string, std::set<int>>& location, const std::map<std::string, int>& words, std::string filename)
+{
+    std::ofstream ost{filename};
+    if(!ost)
+    {
+        throw std::runtime_error("Neisejo atidaryti failo irasymui!");
+        return;
+    }
+
+    for(const auto& [key, value] : words)
+        if(value > 1)
+        {
+            auto location_value = location.find(key);
+            ost << key << ": ";
+            for(const auto& line : location_value->second)
+                ost << line << " ";
+            ost << "\n";
+        }
+
 }
